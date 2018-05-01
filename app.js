d9c222bd150b4cefe0d541116ff36d36d7c5a0d4
babucse1991 <babucse1991@gmail.com>
2018-05-01 22:51:02 +0530

    CROS orgin issue

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,11 +32,7 @@ app.use('/', routes);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
 
 // error handlers
 
